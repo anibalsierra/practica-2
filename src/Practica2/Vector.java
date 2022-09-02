6082ff6c2788dcc63df4ebe115230548a7a3fbdf
Anibal <57988017+anibalsierra@users.noreply.github.com>
2022-09-02 10:08:20 -0500

    Add files via upload

--- src/Practica2/Vector.java
+++ src/Practica2/Vector.java
@@ -10,4 +10,42 @@ package Practica2;
  */
 public class Vector {
     
+    private Object v[];
+    private int n;
+    
+    public Vector(int n){        
+    }
+    public int retornaCapacidad(){
+    }
+    public int retornaNumeroElementos(){
+    }
+    void asignaNumeroElementos(int n){
+    }
+    Object retornaDatoEn(int i){
+    }
+    void asignaDato(Object d, int i){
+    }
+    void muestraDatos(){        
+    }
+    boolean esVacio(){
+    }
+    boolean esLleno(){
+    }
+    int retornaPosicionDato(Object d){
+    }
+    void ordenaAscendente(){
+    }
+    void ordenaDescendente(){
+    }
+    void agregar(Object d){
+    }
+    int buscaDondeInsertar(Object d){
+    }
+    void inserta(Object d, int i){
+    }
+    void inserta(Object d){
+    }
+    void borraDatoEn(int i){
+    }
+    
 }
