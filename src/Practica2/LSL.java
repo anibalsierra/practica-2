21f70bda68f06f5f322c1d399c5d020fd8af2562
wil101 <101955537+wil101@users.noreply.github.com>
2022-09-02 10:17:54 -0500

    se actulizan las dos clases

--- src/Practica2/LSL.java
+++ src/Practica2/LSL.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Practica2;
 
 /**
@@ -9,5 +5,64 @@ package Practica2;
  * @author beto_
  */
 public class LSL {
-    
+
+    private NodoSimple primero, ultimo;
+
+    public LSL() {
+
+    }
+
+    public boolean esVacia() {
+
+    }
+
+    public NodoSimple primerNodo() {
+
+    }
+
+    public NodoSimple ultimoNodo() {
+
+    }
+
+    public NodoSimple anterior(NodoSimple x) {
+
+    }
+
+    public boolean finDeRecorrido(NodoSimple x) {
+
+    }
+
+    public void recorre() {
+
+    }
+
+    public NodoSimple buscaDondeInsertar(Object d) {
+
+    }
+
+    //Object
+    public void insertar(Object d, NodoSimple y) {
+
+    }
+
+    public void conectar(NodoSimple x, NodoSimple y) {
+
+    }
+
+    public NodoSimple buscarDato(Object d, NodoSimple y) {
+
+    }
+
+    public void borrar(NodoSimple x, NodoSimple y) {
+
+    }
+
+    public void desconectar(NodoSimple x, NodoSimple y) {
+
+    }
+
+    public void ordenarAscendentemente() {
+
+    }
+
 }
--- src/Practica2/NodoSimple.java
+++ src/Practica2/NodoSimple.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Practica2;
 
 /**
@@ -9,5 +5,29 @@ package Practica2;
  * @author beto_
  */
 public class NodoSimple {
-    
+
+    private Object dato;
+    private NodoSimple liga;
+
+    public NodoSimple(Object dato) {
+        this.dato = dato;
+        this.liga = null;
+    }
+
+    public Object asignaDato() {
+        return dato;
+    }
+
+    public NodoSimple asignaLiga() {
+        return liga;
+    }
+
+    public void retornaDato(Object dato) {
+        this.dato = dato;
+    }
+
+    public void retornaLiga(NodoSimple liga) {
+        this.liga = liga;
+    }
+
 }
