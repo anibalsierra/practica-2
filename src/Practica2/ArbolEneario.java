87e723645109f49b62aa58b191496ccd8b05c90a
wil101 <101955537+wil101@users.noreply.github.com>
2022-09-16 18:19:22 -0500

    se agrego creación NodoLg

--- src/Practica2/ArbolEneario.java
+++ src/Practica2/ArbolEneario.java
@@ -1,30 +1,28 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Practica2;
 
 import java.util.Stack;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.Queue;
 
+
 /**
  *
  * @author beto_
  */
 public class ArbolEneario extends ListaG {
-    
-    public int altura(NodoLg L) {
+
+    public int altura(NodoLg Lg) {
         NodoLg p;
         int g, h;
         h = 1;
-        if (L == null) {
+        if (Lg == null) {
             return 0;
         }
-        if (L.retornaLiga() == null) {
+        if (Lg.retornaLiga() == null) {
             return 1;
         }
-        p = L;
+        p = Lg;
         while (p != null) {
             if (p.retornaSw() == 1) {
                 g = altura((NodoLg) p.retornaDato());
@@ -36,6 +34,7 @@ public class ArbolEneario extends ListaG {
         }
         return h + 1;
     }
+
     public void ancestrosDeUnDato(String d) {
         int n = 0;
         char c;
@@ -87,6 +86,7 @@ public class ArbolEneario extends ListaG {
             System.out.println("Dato no está en el árbol");
         }
     }
+
     public void muestraPorNivel() {
         Queue cola;
         NodoLg p, q;
@@ -109,6 +109,7 @@ public class ArbolEneario extends ListaG {
             }
         }
     }
+
     public int gradoDeUnDato(String d) {
         int n = 0;
         NodoLg p, q;
@@ -152,4 +153,60 @@ public class ArbolEneario extends ListaG {
         return 0;
 
     }
+    
+    public void hojasArbol(){
+        
+        
+        
+    }
+    
+    public void gradoArbol(){
+        
+    }
+    
+    public void contruirArbolLg(String hilo) {
+        int n, i;
+        NodoLg primero, ultimo, x;
+        Stack pila;
+        pila = new Stack();
+        primero = new NodoLg(null);
+        ultimo = primero;
+        primero.asignaDato(hilo.charAt(2));
+        n = hilo.length();
+        if (n == 3) {
+            return;
+        }
+        i = 4;
+        while (i <= n - 2) {
+            x = new NodoLg(null);
+            ultimo.asignaLiga(x);
+            ultimo = x;
+            if ((hilo.charAt(i + 1) + "").equals("(")) {
+                ultimo.asignaSw(1);
+                pila.push(ultimo);
+                x = new NodoLg(hilo.charAt(i));
+                ultimo.asignaDato(x);
+                ultimo = x;
+                i = i + 2;
+            } else {
+                ultimo.asignaDato(hilo.charAt(i));
+                if ((hilo.charAt(i + 1) + "").equals (")")) {
+                    i = i + 1;
+                    while (i < n && (hilo.charAt(i) + "").equals(")") && !pila.empty()) {
+                        ultimo = (NodoLg) pila.pop();
+                        i = i + 1;
+                    }
+                    if ((hilo.charAt(i) + "").equals(",")) {
+                        i = i + 1;
+                    } else {
+                        i = i + 2;
+                    }
+                }
+
+            }
+
+        }
+
+    }
+    
 }
