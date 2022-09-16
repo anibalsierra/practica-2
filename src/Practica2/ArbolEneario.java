104e64b737ff022e71d6ecf05b42acd1176070e3
wil101 <101955537+wil101@users.noreply.github.com>
2022-09-16 15:44:00 -0500

    se corrigen los diferentes errores

--- src/Practica2/ArbolEneario.java
+++ src/Practica2/ArbolEneario.java
@@ -1,161 +1,155 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Practica2;
+
+import java.util.Stack;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.Queue;
 
 /**
  *
  * @author beto_
  */
-public class ArbolEneario {
-    
-    public int altura(nodoLg L) {
-        nodoLg p;
+public class ArbolEneario extends ListaG {
+
+    public int altura(NodoLg L) {
+        NodoLg p;
         int g, h;
         h = 1;
         if (L == null) {
             return 0;
         }
-        if (L.retornliga() == null) {
+        if (L.retornaLiga() == null) {
             return 1;
         }
         p = L;
         while (p != null) {
-            if (p.retornSw() == 1) {
-                g = altura((nodoLg) p.retornaDato());
+            if (p.retornaSw() == 1) {
+                g = altura((NodoLg) p.retornaDato());
                 if (g > h) {
                     h = g;
                 }
             }
-            p = p.retornLiga();
+            p = (NodoLg) p.retornaLiga();
         }
         return h + 1;
     }
 
     public void ancestrosDeUnDato(String d) {
-        int n = o;
+        int n = 0;
         char c;
-        nodoLg p, q;
+        NodoLg p, q;
         Stack pila = new Stack();
-        p = primerNodo();
+        p = (NodoLg) primerNodo();
         if ((char) p.retornaDato() == d.charAt(0)) {
             System.out.println("el dato es la raiz, por tanto no tiene ancestros");
             return;
         }
-        pila.apilar(p.retornaDato());
-        p = p.retornaLiga();
+        pila.push(p.retornaDato());
+        p = (NodoLg) p.retornaLiga();
         while (p != null) {
-            if (p.retornSw() == 0) {
-                if ((char) p.retornDato() == d.charAt(0)) {
-                    System.out.println("Los ancestros de " + d + " son");
-                    while (!pila.esVacia()) {
-                        c = (char) pila.desapilar();
-                        System.out.println(c + "");
-                        if (!pila.esVacia()) {
-                            p = (nodoLg) pila.desapilar();
+            if (p.retornaSw() == 0) {
+                if ((char) p.retornaDato() == d.charAt(0)) {
+                    System.out.println("Los ancestros de " + d + " son:");
+                    while (!pila.empty()) {
+                        c = (char) pila.pop();
+                        System.out.print(c + ", ");
+                        if (!pila.empty()) {
+                            p = (NodoLg) pila.pop();
                         }
                     }
                     return;
                 }
-                p = p.retornLiga();
-                else
-                q = (nodoLg)p.retornaDato();
+                p = (NodoLg) p.retornaLiga();
+            } else {
+                q = (NodoLg) p.retornaDato();
                 if ((char) q.retornaDato() == d.charAt(0)) {
                     System.out.println("Los ancestros de " + d + " son");
-                    while (!pila.esVacia()) {
-                        c = (char) pila.desapilar();
+                    while (!pila.empty()) {
+                        c = (char) pila.pop();
                         System.out.println(c + "");
-                        if (!pila.esVacia()) {
-                            p = (nodoLg) pila.desapilar();
+                        if (!pila.empty()) {
+                            p = (NodoLg) pila.pop();
                         }
                     }
                     return;
-                    else 
-                    pila.apilar(p.retornaLiga());
-                    pila.apilar(q.retornaDato());
-                    p = q.retornaLiga();
-
+                } else {
+                    pila.push(p.retornaLiga());
+                    pila.push(q.retornaDato());
+                    p = (NodoLg) q.retornaLiga();
                 }
             }
-            while (p == null && !pila.esVacia()) {
-                c = (char) pila.desapilar();
-                p = (nodoLg) pila.desapilar();
+            while (p == null && !pila.empty()) {
+                c = (char) pila.pop();
+                p = (NodoLg) pila.pop();
             }
+            System.out.println("Dato no está en el árbol");
         }
-        System.out.println("Dato no está en el árbol");
-
     }
 
     public void muestraPorNivel() {
-        colas cola;
-        nodoLg p, q;
-        cola = new colas();
-        p = primerNodo();
+        Queue cola;
+        NodoLg p, q;
+        cola = new ConcurrentLinkedQueue();
+        p = (NodoLg) primerNodo();
         if (p == null) {
             return;
         }
         while (p != null) {
-            if (p.retornSw() == 0) {
+            if (p.retornaSw() == 0) {
                 System.out.println((char) p.retornaDato() + " , ");
-                else
-                    q = (nodoLg)p.retornaDato();
+            } else {
+                q = (NodoLg) p.retornaDato();
                 System.out.println((char) q.retornaDato() + " , ");
-                cola.encolar(q.retornaLiga());
+                cola.add(q.retornaLiga());
             }
-            p = p.retornaLiga();
-            while (p == null && !cola.esVacia()) {
-                p = (nodoLg) cola.desencolar();
+            p = (NodoLg) p.retornaLiga();
+            while (p == null && !cola.isEmpty()) {
+                p = (NodoLg) cola.poll();
             }
         }
-
     }
 
     public int gradoDeUnDato(String d) {
         int n = 0;
-        nodoLg p, q;
-        stack pila = new stack();
-        p = primerNodo();
-        if ((char) p.retornDato() == d.charAt(0)) {
-            p = p.retornaLiga();
+        NodoLg p, q;
+        Stack pila = new Stack();
+        p = (NodoLg) primerNodo();
+        if ((char) p.retornaDato() == d.charAt(0)) {
+            p = (NodoLg) p.retornaLiga();
             while (p != null) {
                 n = n + 1;
-                p = p.retornLiga();
+                p = (NodoLg) p.retornaLiga();
             }
             return n;
         }
-        p = p.retornLiga();
+        p = (NodoLg) p.retornaLiga();
         while (p != null) {
             if (p.retornaSw() == 0) {
                 if ((char) p.retornaDato() == d.charAt(0)) {
                     return 0;
                 }
-                p = p.retornLiga();
-                else
-                q = (nodoLg)p.retornDato();
-                if ((char) q.retornDato() == d.charAt(0)) {
-                    p = q.retornLiga();
+                p = (NodoLg) p.retornaLiga();
+            } else {
+                q = (NodoLg) p.retornaDato();
+                if ((char) q.retornaDato() == d.charAt(0)) {
+                    p = (NodoLg) q.retornaLiga();
                     while (p != null) {
                         n = n + 1;
-                        p = p.retornliga();
+                        p = (NodoLg) p.retornaLiga();
 
                     }
                     return n;
-                    else 
-                        pila.apilar(p.retornLiga());
-                    p = q.retornLiga();
-
+                } else {
+                    pila.push(p.retornaLiga());
+                    p = (NodoLg) q.retornaLiga();
                 }
-
             }
-            while (p == null && !pila.esVacia()) {
-                p = (nodoLg) pila.desapilar();
+            while (p == null && !pila.empty()) {
+                p = (NodoLg) pila.pop();
             }
         }
         System.out.println("dato no esta en el arbol");
         return 0;
 
     }
-
-    
 }
